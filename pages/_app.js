0b7bbbf8d0521835e5b74a2258caa6cba852e262
Arif hasan <145915657+Blackeye246@users.noreply.github.com>
2025-08-25 14:16:16 +0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import Header from "../components/Header";
+
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+return (
+<SessionProvider session={session}>
+<Header />
+<main className="container">
+<Component {...pageProps} />
+</main>
+</SessionProvider>
+);
+}
